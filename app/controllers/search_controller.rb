579736f7f202a6002cef7d5da8aaebdae29d5e98
EC2 Default User <ec2-user@ip-172-31-7-8.ec2.internal>
2022-03-20 12:12:43 +0000

    0320-1

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   
   def search
     @posts = Post.joins(:tags).where(tags:{ name: "#{params[:keyword]}"}).distinct.order(created_at: :desc)
+    @tag = Tag.find_by(name: params[:keyword])
   end
   
 end
