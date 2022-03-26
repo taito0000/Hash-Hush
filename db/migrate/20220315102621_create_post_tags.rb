ae5b8a475fe5847f2dbf133c9099772d5b3a4585
taito0000 <taito.bboying@gmail.com>
2022-03-26 12:41:35 +0000

    2022/03/26-2

--- db/migrate/20220315102621_create_post_tags.rb
+++ db/migrate/20220315102621_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[5.2]
   def change
     create_table :post_tags do |t|
-      t.references :post, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.integer :post_id, foreign_key: true
+      t.integer :tag_id, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220315105057_create_follow_tags.rb
+++ db/migrate/20220315105057_create_follow_tags.rb
@@ -1,8 +1,8 @@
 class CreateFollowTags < ActiveRecord::Migration[5.2]
   def change
     create_table :follow_tags do |t|
-      t.references :user, foreign_key: true
-      t.references :tag, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :tag_id, foreign_key: true
       
       t.timestamps
     end
